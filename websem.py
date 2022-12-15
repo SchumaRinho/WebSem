237c57f61999e87cd1bc55ac0362d5422305937c
Skyplay14 <alex.bellebon@live.fr>
2022-12-15 16:13:19 +0100

    rapide correction

--- websem.py
+++ websem.py
@@ -1,5 +1,5 @@
 import spacy
-from offres_emploi import Api #pip install api-offres-emploi
+from offres_emploi import Api
 import numpy
 from numpy import asarray
 
@@ -31,14 +31,13 @@ def scoreSort(tab, tabScore):
         z+=1
         if tmpVal == tab[x][1]:
             dic[tab[x][0]] = tabScore[tab[x][0]]
-            print(z)
         elif (tmpVal > tab[x][1]):
-            print(z)
             dic = sorted(dic.items(), key=lambda t: t[1], reverse=True)
             for y in dic:
                 sort.append(y[0])
             dic={}
             tmpVal = tab[x][1]
+            dic[tab[x][0]] = tabScore[tab[x][0]]
     dic = sorted(dic.items(), key=lambda t: t[1], reverse=True)
     for y in dic:
         sort.append(y[0])
@@ -55,18 +54,22 @@ client = Api(client_id="PAR_toccupes_cd7d5ac5354e7e76ae1a52a04d9c61d9eb6503b5177
              client_secret="e517f83dae8f2a814409e64d3afadf3456317ae15554c9f356c45b56c0cf145d")
 
 
-stop = False
 
 
-
-while not stop:
+while True:
+    recherche = []
     tab=[]
     search = {}
     searchResult = {}
     searchScore = {}
 
-    recherche = [input("Veuillez entrer votre recherche : ")]
+    # Requete utilisateur prenant en compte seulement les noms de la recherche
+    request = input("Veuillez entrer votre recherche : ")
+    for token in nlp(request):
+        if token.tag_ == "NOUN":
+            recherche.append(str(token.text))
     print("")
+    
     # Récuperer les mots similaires a la requete utilisateur
     keyss,_,scoress = nlp.vocab.vectors.most_similar(asarray([nlp(word).vector for word in recherche]),n=100)
     for keys, scores in zip(keyss, scoress):
@@ -84,14 +87,12 @@ while not stop:
             "range":"0-20"
             }
         my_search = client.search(params=arg)
-        x=0
         y=0
-        for i in my_search["resultats"]:    # trier les résultats par leurs nombre d'apparition dans chaque requetes 
+        for x in range(len(my_search["resultats"])):    # trier les résultats par leurs nombre d'apparition dans chaque requetes 
             doc2 = nlp(my_search['resultats'][x]['appellationlibelle']) 
             if (my_search['resultats'][x]["intitule"] in search.keys()) and (my_search['resultats'][x]["description"] == searchResult[my_search['resultats'][x]["intitule"]]["description"]):
                 search[my_search['resultats'][x]["intitule"]] += 1
-                if searchScore[my_search['resultats'][x]["intitule"]] < doc1.similarity(doc2):
-                    searchScore[my_search['resultats'][x]["intitule"]] = doc1.similarity(doc2)
+                searchScore[my_search['resultats'][x]["intitule"]] = doc1.similarity(doc2)
             elif (my_search['resultats'][x]["intitule"] in search.keys()) and (my_search['resultats'][x]["description"] != searchResult[my_search['resultats'][x]["intitule"]]["description"]):
                 search["("+str(y)+")"+str(my_search['resultats'][x]["intitule"])] = 1
                 searchScore["("+str(y)+")"+str(my_search['resultats'][x]["intitule"])] = doc1.similarity(doc2)
@@ -101,14 +102,14 @@ while not stop:
                 search[my_search['resultats'][x]["intitule"]] = 1
                 searchScore[my_search['resultats'][x]["intitule"]] = doc1.similarity(doc2)
                 searchResult[my_search['resultats'][x]["intitule"]] = my_search['resultats'][x]
-            x+=1
+
     search = sorted(search.items(), key=lambda t: t[1], reverse=True)
-    y=0
    
     search = scoreSort(search, searchScore) # trier les doublons de nombre de requete par leurs similarité décroissante
                                             # entre leurs appelationlibelle et la requete utilisateur
 
     y=0
+    print(search)
     for x in search:
         y+=1
         print("[",str(y),"] -",x)
@@ -116,7 +117,8 @@ while not stop:
 
 
     
-    stop = finRequete()
+    if finRequete():
+        break
 
 
 
